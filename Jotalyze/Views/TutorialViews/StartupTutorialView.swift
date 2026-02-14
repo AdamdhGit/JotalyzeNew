1e33270d55c92dec30aeb59c53c88207fb5b625f
Adam <adamdhm@yahoo.com>
2026-02-13 21:20:54 -0800

    added pictures to tutorial view, changed some tutorial view wording

--- Jotalyze/Views/TutorialViews/StartupTutorialView.swift
+++ Jotalyze/Views/TutorialViews/StartupTutorialView.swift
@@ -23,16 +23,20 @@ struct StartupTutorialView: View {
             TabView(selection: $tutorialTabSelected){
                 
                 ZStack{
+                  
+                    
                     VStack{
 
                         Text("Jotalyze").font(.title).fontWeight(.light).padding(.top, 130)
                         
-                        Text("Jot your thoughts and receive analytics.").multilineTextAlignment(.center) .padding(.top, 10).fontWeight(.light)
-                        VStack{
-                            Text("It's journaling,").fontWeight(.light)
-                            +
-                            Text(" but smarter.").bold()
-                        }.padding(.top, 10)
+                        Text("Jot your thoughts and analyze your growth.").multilineTextAlignment(.center) .padding(.top, 10).fontWeight(.light)
+                        
+                        Image(colorScheme == .dark ? "jotalyzeIntroPicDarkMode" : "jotalyzeIntroPic")
+                            .resizable()
+                            
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                          
                         
                         
                         Spacer()
@@ -46,15 +50,22 @@ struct StartupTutorialView: View {
                             }
                         }label:{
                             Text("Get Started").padding(10).padding(.horizontal, 20)
-                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                .foregroundStyle(colorScheme == .dark ? .black : .white)
+                            
+                        }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                     }
                     
                 }.tag(0)
                 
                 ZStack{
-
+                    
                     VStack{
-                        Image(systemName: "sun.max").padding(.top, 130).font(.title)
+                        Image("moodPic")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 300, height: 300).padding(.top, 80)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
                         Text("Optimize Your Mood").bold().padding(.top, 10)
                         Text("Track your mood with Mood Check-In entries. Receive analytics comparing mood trends over time through different graphs.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
                         
@@ -69,7 +80,9 @@ struct StartupTutorialView: View {
                             }
                         }label:{
                             Text("Next").padding(10).padding(.horizontal, 20)
-                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                .foregroundStyle(colorScheme == .dark ? .black : .white)
+                            
+                        }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                     }
                     
                 }.tag(1).padding(.horizontal)
@@ -77,7 +90,13 @@ struct StartupTutorialView: View {
                 ZStack{
       
                     VStack{
-                        Image(systemName: "trophy.fill").padding(.top, 130).font(.title)
+                        Image("trophiesPic")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 300, height: 300).padding(.top, 80)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        
                         Text("Track Goals").padding(.top, 10).bold()
                         Text("Create goals, log progress, reflect on what worked and what didn't, and gain valueable insights through analytics tracking completions and hours spent on tasks to help you achieve your objectives.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
                         
@@ -91,7 +110,9 @@ struct StartupTutorialView: View {
                             }
                         }label:{
                             Text("Next").padding(10).padding(.horizontal, 20)
-                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                .foregroundStyle(colorScheme == .dark ? .black : .white)
+                            
+                        }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                     }
                     
                 }.tag(2).padding(.horizontal)
@@ -100,7 +121,13 @@ struct StartupTutorialView: View {
              
                     VStack{
                         
-                        Image(systemName: "checkmark.circle.fill").padding(.top, 130).font(.title)
+                        Image("checkmarksPic")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 300, height: 300).padding(.top, 80)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        
                         Text("Build Habits").padding(.top, 10).bold()
                         Text("Set habits and check off daily completions to build a routine that supports the lifestyle you want to live.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
                         
@@ -117,14 +144,23 @@ struct StartupTutorialView: View {
                             }
                         }label:{
                             Text("Next").padding(10).padding(.horizontal, 20)
-                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                .foregroundStyle(colorScheme == .dark ? .black : .white)
+                            
+                        }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                     }
                 }.tag(3).padding(.horizontal)
                 
                 ZStack{
               
                     VStack{
-                        Image(systemName: "chart.pie.fill").padding(.top, 130).font(.title)
+                        
+                        Image(colorScheme == .dark ? "chartsPicDarkMode" : "chartsPicLightMode")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 300, height: 300).padding(.top, 80)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        
                         Text("Gain Insights").padding(.top, 10).bold()
                         Text("Chart your journey, uncover mood patterns, dive into rich analytics, and visualize your progress as you achieve your goals.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
                         
@@ -142,107 +178,120 @@ struct StartupTutorialView: View {
                             }
                         }label:{
                             Text("Next").padding(10).padding(.horizontal, 20)
-                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                .foregroundStyle(colorScheme == .dark ? .black : .white)
+                            
+                        }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                     }
                 }.tag(4).padding(.horizontal)
                 
          
-                    ZStack{
-                     
-                        ScrollView {
-                            
-                            Image(systemName: "lock.shield").padding(.top, 80).font(.title)
-                            Text("Secure Journaling").padding(.top, 10).bold()
-                            Text("Your privacy is our priority. Entries are locally encrypted, and you can secure them with Face ID, Touch ID, or a passcode.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
-                            
-                            Spacer()
-                        }.scrollIndicators(.hidden)
+                ZStack{
+                    
+                    ScrollView {
                         
+                        Image("SecurityPic")
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 300, height: 300).padding(.top, 80)
+                            .foregroundColor(colorScheme == .dark ? .white : .black)
+                        
+                        Text("Secure Journaling").padding(.top, 10).bold()
+                        Text("Your privacy is our priority. Entries are locally encrypted and never leave your device. You can also secure them with Face ID, Touch ID, or a passcode.").padding(.top, 10).fontWeight(.light).multilineTextAlignment(.center)
+                        
+                        Text("No ads. No tracking. No data selling — ever.").padding(.top, 10).fontWeight(.bold).multilineTextAlignment(.center)
+                        
+                        Spacer()
+                    }.scrollIndicators(.hidden)
+                    
+                    VStack{
+                        Spacer()
                         VStack{
-                            Spacer()
-                            VStack{
-                                Button{
-                                    withAnimation{
-                                        tutorialTabSelected = 6
-                                    }
-                                }label:{
-                                    Text("Next").padding(10).padding(.horizontal, 20)
-                                }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
-                            }
-                        }
-                        
-                    }.tag(5).padding(.horizontal)
-                
-       
-                    ZStack{
-                        
-                        
-                        
-                        ScrollView {
-                            Image(systemName: "rectangle.fill.on.rectangle.fill").padding(.top, 20).font(.title)
-                            Text("6 Journal Styles").padding(.top, 10).bold()
-                            
-                            
-                            
-                            VStack{
-                                Text("Morning Preparation").padding(.bottom, 3)
-                                Text("Set positive intentions for the day.").fontWeight(.light).multilineTextAlignment(.center).font(.subheadline).opacity(0.75)
-                                
-                                Divider().padding(.vertical, 4)
-                                
-                                Text("Evening Reflection").padding(.bottom, 3)
-                                Text("Reflect on your days intentions.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
-                                
-                                Divider().padding(.vertical, 4)
-                                
-                                Text("Mood Check-In").padding(.bottom, 3)
-                                Text("Track your emotions and receive weekly analyses.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
-                                
-                                Divider().padding(.vertical, 4)
-                                
-                                Text("Gratitude").padding(.bottom, 3)
-                                Text("Note what you're thankful for.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
-                                
-                                
-                                Divider().padding(.vertical, 4)
-                                
-                                Text("Goal Tracking").padding(.bottom, 3)
-                                Text("Track goals with weekly analyses.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
-                                
-                                Divider().padding(.vertical, 4)
-                                
-                                Text("Capture the Moment").padding(.bottom, 3)
-                                Text("Reflect on meaningful experiences.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
-                                
-                            }.padding().overlay{
-                                RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 0.5).foregroundStyle(.gray)
-                            }.scaleEffect(0.9)
-                            Spacer().frame(height: 150)
-                        }.padding().scrollIndicators(.hidden)
-                        
-                        VStack{
-                            Spacer()
-                            ZStack{
-                                Rectangle().foregroundStyle(colorScheme == .light ? Color.white : Color.black).frame(height: 160)
-                                Rectangle().foregroundStyle(Color.gray.opacity(0.1)).frame(height: 160)
-                            }
-                        }
-                        
-                        VStack{
-                            Spacer()
                             Button{
                                 withAnimation{
-                                    startupTutorialDone.tutorialIsDone = true
+                                    tutorialTabSelected = 6
                                 }
                             }label:{
-                                Text("Start Journaling").padding(10).padding(.horizontal, 20)
-                            }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                                Text("Next").padding(10).padding(.horizontal, 20)
+                                    .foregroundStyle(colorScheme == .dark ? .black : .white)
+                                
+                            }.buttonStyle(.borderedProminent).tint(colorScheme == .dark ? .white : .black).padding(.bottom, 100)
                         }
+                    }
+                    
+                }.tag(5).padding(.horizontal)
+                
+       
+                ZStack{
+                    
+                    
+                    
+                    ScrollView {
+                        Image(systemName: "rectangle.fill.on.rectangle.fill").padding(.top, 20).font(.title)
+                        Text("6 Journal Styles").padding(.top, 10).bold()
                         
-                    }.tag(6).padding(.horizontal)
+                        
+                        
+                        VStack{
+                            Text("Morning Preparation").padding(.bottom, 3)
+                            Text("Set positive intentions for the day.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("Evening Reflection").padding(.bottom, 3)
+                            Text("Reflect on your days intentions.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("Mood Check-In").padding(.bottom, 3)
+                            Text("Track your mood and receive valuable analytics.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("Gratitude").padding(.bottom, 3)
+                            Text("Note what you're thankful for.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                            
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("Goal Tracking").padding(.bottom, 3)
+                            Text("Track your goals with clear visual progress.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("Capture the Moment").padding(.bottom, 3)
+                            Text("Reflect on meaningful experiences.").fontWeight(.light).multilineTextAlignment(.center).opacity(0.75)
+                            
+                        }.padding().overlay{
+                            RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 0.5).foregroundStyle(.gray)
+                        }.scaleEffect(0.9)
+                        Spacer().frame(height: 150)
+                    }.padding().scrollIndicators(.hidden)
+                    
+                    VStack{
+                        Spacer()
+                        ZStack{
+                            Rectangle().foregroundStyle(colorScheme == .light ? Color.white : Color.black).frame(height: 160)
+                            Rectangle().foregroundStyle(Color.gray.opacity(0.1)).frame(height: 160)
+                        }
+                    }
+                    
+                    VStack{
+                        Spacer()
+                        Button{
+                            withAnimation{
+                                startupTutorialDone.tutorialIsDone = true
+                            }
+                        }label:{
+                            Text("Start Journaling").padding(10).padding(.horizontal, 20)
+                        }.buttonStyle(.borderedProminent).tint(.black).padding(.bottom, 100)
+                    }
+                    
+                }.tag(6).padding(.horizontal)
                 
             }
-            .tabViewStyle(PageTabViewStyle()).ignoresSafeArea().onAppear{
+            .tabViewStyle(PageTabViewStyle())
+            .onAppear{
                 UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(.primary)
                        UIPageControl.appearance().pageIndicatorTintColor = UIColor(.secondary)
             }
